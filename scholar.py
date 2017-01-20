634ab3c344043e29ab077a4b5a90b32aa98ce716
Sean Carmody <seancarmody@gmail.com>
2017-01-21 09:55:01 +1100

    Specify Beautiful Soup parsing method

--- scholar.py
+++ scholar.py
@@ -359,7 +359,7 @@ class ScholarArticleParser(object):
         content as needed, and notifies the parser instance of
         resulting instances via the handle_article callback.
         """
-        self.soup = BeautifulSoup(html)
+        self.soup = BeautifulSoup(html, 'lxml')
 
         # This parses any global, non-itemized attributes from the page.
         self._parse_globals()
